dd9d86413a78da0f44884939064afcbb163ac71b
Tom Lausmann <tomlausmann@gmail.com>
2023-12-23 21:53:20 +0100

    Initial Commit

---
+++ src/main/kotlin/de/lausi95/gsvrankedwatchergamewatcher/GsvRankedWatcherGameWatcherApplication.kt
@@ -0,0 +1,11 @@
+package de.lausi95.gsvrankedwatchergamewatcher
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GsvRankedWatcherGameWatcherApplication
+
+fun main(args: Array<String>) {
+  runApplication<GsvRankedWatcherGameWatcherApplication>(*args)
+}
---
+++ src/test/kotlin/de/lausi95/gsvrankedwatchergamewatcher/GsvRankedWatcherGameWatcherApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.lausi95.gsvrankedwatchergamewatcher
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GsvRankedWatcherGameWatcherApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
