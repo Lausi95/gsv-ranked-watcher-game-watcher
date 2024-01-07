fb9cdcc55c4a45d370ead66d691f041205d0e73d
Tom Lausmann <tomlausmann@gmail.com>
2024-01-07 23:54:03 +0100

    fix: replace player list with correct player collection

--- src/main/kotlin/de/lausi95/gsvrankedwatchergamewatcher/adapter/playermanager/PlayerManagerPlayerRepository.kt
+++ src/main/kotlin/de/lausi95/gsvrankedwatchergamewatcher/adapter/playermanager/PlayerManagerPlayerRepository.kt
@@ -4,10 +4,14 @@ import de.lausi95.gsvrankedwatchergamewatcher.domain.model.player.Player
 import de.lausi95.gsvrankedwatchergamewatcher.domain.model.player.PlayerRepository
 import org.springframework.web.client.RestTemplate
 
+private data class PlayerCollection(
+  val players: List<Player>
+)
+
 internal class PlayerManagerPlayerRepository(private val restTemplate: RestTemplate): PlayerRepository {
 
   override fun getPlayers(): List<Player> {
-    val response = restTemplate.getForEntity("/players", Array<Player>::class.java)
-    return response.body?.toList()!!
+    val response = restTemplate.getForEntity("/players", PlayerCollection::class.java)
+    return response.body?.players!!
   }
 }
