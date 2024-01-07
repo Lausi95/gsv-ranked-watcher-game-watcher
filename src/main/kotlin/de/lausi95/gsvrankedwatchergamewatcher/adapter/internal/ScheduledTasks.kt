677fe6e4f379ebcd2d4cca55b8878ad69bdace9c
Tom Lausmann <tomlausmann@gmail.com>
2024-01-07 21:43:26 +0100

    Add logging to scheduled tasks

--- src/main/kotlin/de/lausi95/gsvrankedwatchergamewatcher/adapter/internal/ScheduledTasks.kt
+++ src/main/kotlin/de/lausi95/gsvrankedwatchergamewatcher/adapter/internal/ScheduledTasks.kt
@@ -1,15 +1,19 @@
 package de.lausi95.gsvrankedwatchergamewatcher.adapter.internal
 
 import de.lausi95.gsvrankedwatchergamewatcher.application.MatchApplicationService
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.util.concurrent.TimeUnit
 
 @Component
-private class ScheduledTasks(val matchApplicationService: MatchApplicationService) {
+private class ScheduledTasks(private val matchApplicationService: MatchApplicationService) {
+
+  private val log = LoggerFactory.getLogger(ScheduledTasks::class.java)
 
   @Scheduled(fixedDelay = 5, initialDelay = 1, timeUnit = TimeUnit.MINUTES)
   fun crawlMatches() {
+    log.info("Crawling Matches")
     matchApplicationService.crawlMatches()
   }
 }
