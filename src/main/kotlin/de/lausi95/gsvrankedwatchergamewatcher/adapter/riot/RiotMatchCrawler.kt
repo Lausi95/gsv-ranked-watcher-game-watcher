70b667f1ce0bae1c4e89f4fada5b250c323d40b5
Tom Lausmann <tom.lausmann@unitb.de>
2024-01-08 15:08:21 +0100

    chore: add logging

--- src/main/kotlin/de/lausi95/gsvrankedwatchergamewatcher/adapter/riot/RiotMatchCrawler.kt
+++ src/main/kotlin/de/lausi95/gsvrankedwatchergamewatcher/adapter/riot/RiotMatchCrawler.kt
@@ -3,6 +3,7 @@ package de.lausi95.gsvrankedwatchergamewatcher.adapter.riot
 import de.lausi95.gsvrankedwatchergamewatcher.domain.model.match.Match
 import de.lausi95.gsvrankedwatchergamewatcher.domain.model.match.MatchCrawler
 import de.lausi95.gsvrankedwatchergamewatcher.domain.model.match.Participant
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
@@ -12,23 +13,27 @@ private class RiotMatchCrawler(private val riotAdapter: RiotAdapter) : MatchCraw
     private val QUEUE_IDS = listOf(420, 440)
   }
 
+  private val log = LoggerFactory.getLogger(RiotMatchCrawler::class.java)
+
   override fun crawlMatches(summonerIds: List<String>, shouldReportMatch: (matchId: String) -> Boolean, reportMatch: (Match) -> Unit) {
     summonerIds.flatMap { summonerId -> riotAdapter.getLatestMatchIds(summonerId) }.distinct().forEach { matchId ->
       if (!shouldReportMatch(matchId)) {
+        log.info("Not Reporting match $matchId. Already reported.")
         return
       }
 
       val riotMatch = riotAdapter.getMatch(matchId)
       if (!QUEUE_IDS.contains(riotMatch.info.queueId)) {
+        log.info("Not reporting match $matchId. Not a ranked match.")
         return
       }
 
-      val match: Match = mapToMatch(riotMatch, summonerIds)
+      val match: Match = mapToMatch(matchId, riotMatch, summonerIds)
       reportMatch(match)
     }
   }
 
-  fun mapToMatch(matchDto: MatchDto, summonerIds: List<String>): Match {
+  fun mapToMatch(matchId: String, matchDto: MatchDto, summonerIds: List<String>): Match {
     val participantDtos = matchDto.info.participants.filter {
       summonerIds.contains(it.puuid)
     }
@@ -49,6 +54,6 @@ private class RiotMatchCrawler(private val riotAdapter: RiotAdapter) : MatchCraw
       )
     }
 
-    return Match(matchDto.info.gameId.toString(), win, participants)
+    return Match(matchId, win, participants)
   }
 }
