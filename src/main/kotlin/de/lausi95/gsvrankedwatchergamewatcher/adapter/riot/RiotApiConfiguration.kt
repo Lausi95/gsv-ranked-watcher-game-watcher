82eb70ca875a167bb31440ddea7ecdfbf1632bcb
Tom Lausmann <tom.lausmann@unitb.de>
2024-01-08 10:46:44 +0100

    chore: refactor RiotAdapterConfiguration

--- src/main/kotlin/de/lausi95/gsvrankedwatchergamewatcher/adapter/riot/RiotApiConfiguration.kt
+++ src/main/kotlin/de/lausi95/gsvrankedwatchergamewatcher/adapter/riot/RiotApiConfiguration.kt
@@ -1,19 +1,15 @@
 package de.lausi95.gsvrankedwatchergamewatcher.adapter.riot
 
-import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-private class RiotApiConfiguration(@Value("\${riot.api-key}") val apiKey: String) {
+private class RiotApiConfiguration {
 
   @Bean
-  fun riotAdapter(objectMapper: ObjectMapper): RiotAdapter {
-    objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-
+  fun riotAdapter(@Value("\${riot.api-key}") apiKey: String): RiotAdapter {
     val restTemplate = RestTemplateBuilder()
       .defaultHeader("X-Riot-Token", apiKey)
       .rootUri("https://europe.api.riotgames.com")
